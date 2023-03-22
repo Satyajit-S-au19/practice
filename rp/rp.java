a2b69908bb8a023a3dbebcede3f1eba3f04e20c8
New Developer <slenka528@gmail.com>
2023-03-22 11:13:05 +0530

    this is the second commit

--- rp/rp.java
+++ rp/rp.java
@@ -1,5 +1,7 @@
 public class rp{
     public static void main(String[] args) {
         System.out.println("this is for git hub learn");
+
+        System.out.println("this is modified data");
     }
 }
